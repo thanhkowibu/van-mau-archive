7f93c3113c7642a83629e0baebd76d0d24237b69
thanhkowibu <131281853+thanhkowibu@users.noreply.github.com>
2024-08-08 11:15:57 +0700

    remove unused function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,8 @@ import VMNavbar from "./components/common/navbar/VMNavbar";
 import CardList from "./components/common/posts/CardList";
 import { ToasterProvider } from "./providers/ToasterProvider";
 import { GlobalContextProvider } from "./context/useAuth";
-import usePostApi from "./api/modules/post.api";
 
 function App() {
-  const { getProtectedPosts } = usePostApi();
-  const handleClick = async () => {
-    try {
-      const res = await getProtectedPosts();
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-    }
-  };
   return (
     <ToasterProvider>
       <GlobalContextProvider>
