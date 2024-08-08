05b129944f973d5050a48e138b433e4eee9c3901
thanhkowibu <131281853+thanhkowibu@users.noreply.github.com>
2024-08-08 12:41:53 +0700

    change order of posts

--- api/src/services/postsServices.ts
+++ api/src/services/postsServices.ts
@@ -3,7 +3,8 @@ import db from "../db/index";
 export const serviceGetPosts = async () => {
   const q = `SELECT p.*, u.username 
               FROM vm_posts p JOIN vm_users u 
-              ON p.uid = u.id`;
+              ON p.uid = u.id
+              ORDER BY p.id desc`;
 
   return db.query(q);
 };
